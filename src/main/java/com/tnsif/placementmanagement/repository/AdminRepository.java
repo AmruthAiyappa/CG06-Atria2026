7cd7486cedc0c39ec1924e74bfb8f94133de6afe
AmruthAiyappa <167874488+AmruthAiyappa@users.noreply.github.com>
2026-07-09 22:52:41 +0530

    Delete src/main directory

--- src/main/java/com/tnsif/placementmanagement/repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tnsif.placementmanagement.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.tnsif.placementmanagement.entity.Admin;
-
-public interface AdminRepository extends CrudRepository<Admin, Integer> {
-
-}
